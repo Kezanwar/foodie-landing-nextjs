fd407e23f7627df079bfb84eca30e8fb7b4a07cc
Kez Anwar <kezanwar@gmail.com>
2023-10-01 19:19:55 +0100

    features/partner-page started news and insights

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -26,8 +26,12 @@ export default function Index({ allPosts, preview }) {
         <title>{'News & Insights | Foodie'}</title>
       </Head>
       <Spacer />
+
       <Container>
-        <div className="w-full h-[400px] rounded-2xl mb-4">
+        <Typography mb={2} component={'h1'} variant="h2">
+          News & Insights
+        </Typography>
+        <div className="grid  md:grid-cols-2 lg:grid-cols-3 gap-8">
           <Image
             alt="featured-post"
             src={
@@ -36,28 +40,31 @@ export default function Index({ allPosts, preview }) {
             width={0}
             height={0}
             sizes="100vw"
-            className="rounded-2xl"
+            className="rounded-xl"
             style={{ width: '100%', height: '100%', objectFit: 'cover' }} // optional
           />
-        </div>
-        <Typography mb={1} variant="h2">
-          Featured post
-        </Typography>
-        <Typography color={'text.secondary'} variant="body2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe debitis
-          molestias nulla ipsum ex sint nesciunt placeat possimus quae
-          exercitationem rerum, ea dolor aliquam vitae repudiandae, laboriosam
-          inventore cumque, aspernatur pariatur itaque minus? Repudiandae
-          obcaecati praesentium deserunt est laboriosam sequi voluptas dolores
-          placeat dolore. Consequatur qui nam animi facere commodi.
-        </Typography>
-      </Container>
-      <Spacer sp={16} />
-      <Container>
-        <Typography mb={3} variant="h2">
-          News & Insights
-        </Typography>
-        <div className="grid grid-cols-3 gap-8">
+          <Image
+            alt="featured-post"
+            src={
+              'https://www.creativetourist.com/app/uploads/2015/10/10945572_919096154791470_3286054712372461805_n.jpg'
+            }
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="rounded-xl"
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }} // optional
+          />
+          <Image
+            alt="featured-post"
+            src={
+              'https://www.creativetourist.com/app/uploads/2015/10/10945572_919096154791470_3286054712372461805_n.jpg'
+            }
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="rounded-xl"
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }} // optional
+          />
           <Image
             alt="featured-post"
             src={
