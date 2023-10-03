31e3bcf176d0b1f57487c14fcbd4c31d09cc1046
Kez Anwar <kezanwar@gmail.com>
2023-10-03 17:36:56 +0100

    page/single-blog-template more styling updates

--- components/posts/cover-image.tsx
+++ components/posts/cover-image.tsx
@@ -15,19 +15,20 @@ interface Props {
 export default function CoverImage({ title, coverImage, slug }: Props) {
   const image = (
     <Image
-      width={2000}
-      height={1000}
+      width={1000}
+      height={200}
+      style={{ width: '100%', height: '100%', objectFit: 'cover' }} // optional
       alt={`Cover Image for ${title}`}
       src={coverImage?.node.sourceUrl}
-      className={cn('rounded-xl', {
+      className={cn('rounded-xl ', {
         'hover:shadow-medium transition-shadow duration-200': slug
       })}
     />
   );
   return (
-    <div className="sm:mx-0">
+    <div className="h-[280px] lg:h-[400px]">
       {slug ? (
-        <Link href={`/posts/${slug}`} aria-label={title}>
+        <Link href={`/news/${slug}`} aria-label={title}>
           {image}
         </Link>
       ) : (
