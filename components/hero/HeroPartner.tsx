336911705c6377d979a1248098a66102dfe5647d
Kez Anwar <kezanwar@gmail.com>
2023-10-03 23:30:33 +0100

    page/partners - updated link styling

--- components/hero/HeroPartner.tsx
+++ components/hero/HeroPartner.tsx
@@ -58,7 +58,7 @@ const HeroPartner: FC<Props> = () => {
           href={`${RESTAURANT_DASHBOARD_URL}/login`}
         >
           <BlackButton variant="outlined" color="inherit">
-            Login <KeyboardArrowRightIcon sx={{ ml: 0.5 }} fontSize="small" />
+            Login <KeyboardArrowRightIcon sx={{ ml: 0 }} fontSize="small" />
           </BlackButton>
         </Link>
       </div>
